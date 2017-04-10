6fb3197ef3de724db00bddc6b96a5462cca593aa
Hiroki Mizukami <mizukami_hiroki@cyberagent.co.jp>
2017-04-10 22:34:21 +0900

    imprement linearmodel

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
 import skstan.utils.functions
+from skstan.linear_models.linear_models import LinearRegression
+import numpy as np
 
 if __name__ == '__main__':
-    print(skstan.utils.functions.get_hello())
+
+    x = np.array([[1,2,3], [6,5,4]])
+    y = np.array([6,5])
+
+    rgr = LinearRegression()
+
+    stanfit = rgr.fit(x, y)
+
+    print(stanfit)
--- skstan/linear_models/linear_models.py
+++ skstan/linear_models/linear_models.py
@@ -1,17 +1,49 @@
-from abc import ABCMeta
+from abc import ABCMeta, abstractmethod
+import numpy as np
+import pystan as ps
 
-import six
-from sklearn.base import BaseEstimator
-from sklearn.linear_model.base import LinearClassifierMixin
+class RegressionModelMixin():
+    model_code: str
+
+    
+
+    def fit(self, x: np.array, y: np.array):
+        assert len(y.shape) == 1
+        assert len(x.shape) == 2
+        data = {
+            'x': x,
+            'y': y,
+            'n': x.shape[0],
+            'f': x.shape[1],
+        }
+
+        return ps.stan(model_code=self.model_code, data=data)
 
 
-class SampledParametersMixin(ABCMeta):
-    pass
+class LinearRegression(RegressionModelMixin):
+    model_code = '''
+        data{
+            int n;
+            int f;
+            matrix[n,f] x;
+            vector[n] y;
+        }
+        parameters{
+            vector[f] alpha;
+            real beta;
+            real<lower=0> sigma;
+        }
+        transformed parameters{
+            vector[n] yp;
 
+            yp <- x*alpha + beta;
+        }
+        model{
+            alpha ~ normal(0, 100);
+            beta ~ normal(0, 100);
+            sigma ~ uniform(0, 10);
 
-class SampledCoefficientsMixin(SampledParametersMixin):
-    pass
+            y ~ normal(yp, sigma);
+        }
+    '''
 
-
-class LinearRegression(six.with_metaclass(BaseEstimator, LinearClassifierMixin)):
-    pass
