fd338c7a1e3e5dbf6db60ec2199e6870df76364e
stenoritama <shr.nrht.0623@gmail.com>
2017-10-08 15:03:36 +0900

    fix setup.py

--- setup.py
+++ setup.py
@@ -5,10 +5,24 @@ from setuptools import setup, find_packages
 sys.path.append('./skstan')
 sys.path.append('./test')
 
+
+def readme():
+    try:
+        with open('README.md') as f:
+            readme = f.read()
+            return readme
+    except IOError:
+        return ''
+
+
 setup(
     name='skstan',
     version='0.0',
     packages=find_packages(exclude=['test*']),
+    description='',
+    long_description=readme(),
+    author='scikit-stan development team',
+    author_email='scikit-stan@googlegroups.com',
     test_suite='test',
     package_data={
         '': ['*.yaml']
@@ -19,5 +33,11 @@ setup(
         'pandas',
         'pystan',
         'pyyaml'
-    ]
+    ],
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    license="MIT"
 )
