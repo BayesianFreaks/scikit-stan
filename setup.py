d1250bda593ab959027d4cfe921bcd45cab56dd2
stenoritama <shr.nrht.0623@gmail.com>
2017-10-09 12:07:45 +0900

    fix version number

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ description = "Various bayesian models based on stan and pystan with a elegant i
 
 setup(
     name='skstan',
-    version='0.0.0-test',
+    version='0.0.0_0',
     url='https://skstan.org/latest/doc/',
     packages=find_packages(exclude=['test*']),
     description=description,
