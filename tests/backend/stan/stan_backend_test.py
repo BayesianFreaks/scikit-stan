bfe0e2a670194673e7fa18b2f60e9b583f74738f
sdual <shr.nrht.0623@gmail.com>
2019-01-21 14:20:54 +0900

    move variable for test root path to __init__ file

--- tests/backend/stan/stan_backend_test.py
+++ tests/backend/stan/stan_backend_test.py
@@ -1,14 +1,14 @@
 from pystan import StanModel
 
 from skstan.backend.stan import StanBackend
-from tests.test_base import TEST_DIR_ROOT
+from tests import TEST_ROOT
 
 
 class TestStanBackend:
 
     def test_load_stan_model(self):
         # Test that a pickled stan model is loaded and deserialized.
-        StanBackend.PKL_BASE_DIR = TEST_DIR_ROOT + '/test_model'
+        StanBackend.PKL_BASE_DIR = TEST_ROOT + '/test_model'
         model_name = 'linear_regression'
         actual_model = StanBackend.load_stan_model(model_name)
 
