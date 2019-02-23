5004d91796dbae13d7a63badfd6ae4453d9befed
sdual <shr.nrht.0623@gmail.com>
2019-02-24 07:31:06 +0900

    remove distribution

--- skstan/backend/tfp/tfp_backend.py
+++ skstan/backend/tfp/tfp_backend.py
@@ -1,5 +1,3 @@
-import tensorflow_probability as tfp
-
 from skstan.backend.tfp.model import TFPLinearRegression
 from skstan.backend.tfp.model import TFPLogisticRegression
 from skstan.backend.tfp.model import TFPPoissonRegression
@@ -12,52 +10,7 @@ class TFPBackend:
     Probability.
     """
 
+    # Linear Regression.
     LinearRegression = TFPLinearRegression
     LogisitcRegression = TFPLogisticRegression
     PoissionRegression = TFPPoissonRegression
-
-
-_tfd = tfp.distributions
-
-
-class TFDistribution:
-    """
-    TensorFlow Distribution class.
-
-    This class provides typical distributions.
-    """
-
-    @staticmethod
-    def normal(mean, std) -> _tfd.Normal:
-        """
-        Normal distribution object.
-
-        Parameters
-        ----------
-        mean: float or int
-            The mean value of Normal distribution.
-        std: float or int
-            The standard deviation of Normal distribution.
-        Returns
-            The Normal distribution instance of TensorFlow Probability.
-        -------
-
-        """
-        pass
-
-    @staticmethod
-    def bernoulli(probs=()) -> _tfd.Bernoulli:
-        """
-
-        Parameters
-        ----------
-        probs
-
-        Returns
-        -------
-
-        """
-        pass
-
-    def binomial(self) -> _tfd.Binomial:
-        pass
